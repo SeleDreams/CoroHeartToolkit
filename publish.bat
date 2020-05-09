d2c8f7fa6c94bca2068209cce163b4f728ff84c2
SeleDreams <lamcilaktheo@gmail.com>
2020-05-09 08:41:32 +0200

    Added osx to supported platforms

--- publish.bat
+++ publish.bat
@@ -1,4 +1,5 @@
 dotnet publish -r win-x64 -c Release /p:PublishSingleFile=true -o "Publish/win-x64"
 dotnet publish -r win-x86 -c Release /p:PublishSingleFile=true -o "Publish/win-x86"
 dotnet publish -r linux-x64 -c Release /p:PublishSingleFile=true -o "Publish/linux-x64"
+dotnet publish -r osx-x64 -c Release /p:PublishSingleFile=true -o "Publish/osx-x64"
 ./zip.bat
